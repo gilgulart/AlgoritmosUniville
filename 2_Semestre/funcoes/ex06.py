7b3de5d0c6915eda3ec2543465e5503debf80089
gilberto <gilbertogulart70@gmail.com>
2026-08-06 19:56:08 -0300

    Funções, recursão, pesquisa binária

---
+++ 2_Semestre/funcoes/ex06.py
@@ -0,0 +1,7 @@
+def is_peer(num):
+    status =  True if num % 2 == 0 else False
+    return status
+
+
+print(is_peer(2))
+print(is_peer(3))
---
+++ 2_Semestre/funcoes/ex07.py
@@ -0,0 +1,8 @@
+def fatorial(num: int):
+    if num == 0 or num == 1:
+        return 1
+    
+    if num < 0:
+        return "valor inválido"
+    
+    return num * fatorial(num - 1)
